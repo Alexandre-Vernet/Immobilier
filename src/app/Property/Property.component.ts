3fd6d26aab94442278113697717cf3416a1ae74b
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-02-23 17:28:04 +0100

    Add property dynamically

--- src/app/Property/Property.component.ts
+++ src/app/Property/Property.component.ts
@@ -7,11 +7,48 @@ import { PropertyService } from '../Property.service';
   styleUrls: ['./Property.component.scss'],
 })
 export class PropertyComponent implements OnInit {
-  properties: any = [];
+  properties: any;
+  solde: number = 0;
+
+  name: string = '';
+  address: string = '';
+  price: number = 0;
+  owner: string = '';
+  description: string = '';
 
   constructor(private propertyService: PropertyService) {}
 
   ngOnInit() {
     this.properties = this.propertyService.properties;
   }
+
+  getSolde = () => {
+    // Find in array where name = person logged and some solde
+  };
+
+  addProperty = (
+    name: string,
+    address: string,
+    price: number,
+    owner: string,
+    description: string
+  ) => {
+    // Find id for this property
+    let id = this.properties.length;
+
+    // Get property
+    let property = {
+      id: id,
+      name: name,
+      address: address,
+      price: price,
+      owner: owner,
+      description: description,
+    };
+
+    // Add property to array
+    this.properties.push(property);
+
+    console.log(property);
+  };
 }
